dc75a6c38978d277b467134b1b96eb5d072f48b6
adamjkelley01 <adamjkelley01@gmail.com>
2024-01-18 15:41:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MissionAssignment2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to the dice throwing simulator!");
+            System.Console.WriteLine("How many dice would you like to simulate?");
+
+            int numberOfRolls = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            System.Console.WriteLine("Each '*' represents 1% of the total number of rolls");
+            System.Console.WriteLine($"Total number of rolls = {numberOfRolls}");
+
+            DiceSimulator simulator = new DiceSimulator();
+            int[] finalresults = simulator.SimulateRolls(numberOfRolls);
+
+            PrintHistogram(finalresults, numberOfRolls);
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+        }
+
+        private static void PrintHistogram(int[] results, int totalRolls)
+        {
+            for (int i = 2; i <= 12; i++)
+            {
+                int percentage = results[i] * 100 / totalRolls;
+                string asterisks = new string('*', percentage);
+                Console.WriteLine($"{i}:{asterisks}");
+
+            }
+
+        }
+    }
+}
+
+
